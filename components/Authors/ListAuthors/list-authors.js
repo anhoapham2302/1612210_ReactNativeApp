d1063a68a90134c02c123e672edcb85f7c2f9a77
anhoapham2302 <hoaan31071998@gmail.com>
2020-08-25 18:45:50 +0700

    Update list-authors.js

--- components/Authors/ListAuthors/list-authors.js
+++ components/Authors/ListAuthors/list-authors.js
@@ -9,7 +9,6 @@ import {
 import ListAuthorsItem from "../ListAuthorsItem/list-authors-item";
 import Styles from "../../../global/style";
 import { apiGetListAuthor } from "../../../core/services/author-service";
-import AuthorListSearch from "../AuthorListSearch/author-list-search";
 import { LanguageContext } from "../../../provider/language-provider";
 import { ThemeContext } from "../../../provider/theme-provider";
 
@@ -23,29 +22,15 @@ const ListAuthor = (props) => {
       .then((res) => setData(res.payload))
       .catch((err) => console.log(err));
   }, []);
-const onPressMore = () => {
-    <AuthorListSearch navigation={props.navigation} data={data} com = "ListAuthor"/>
-}
+
   return (
     <View style={Styles.view}>
       <View style={{ flexDirection: "row" , justifyContent: "space-between"}}>
         <Text style={[Styles.text, { fontSize: 20, color: theme.foreground }]}>{props.title}</Text>
-        <TouchableOpacity>
-          <Text
-            style={{
-              marginRight: 15,
-              marginTop: 5,
-              fontSize: 15,
-              color: "#4DC4FF",
-            }}
-          >
-            {language.more} {">>"}
-          </Text>
-        </TouchableOpacity>
       </View>
       <FlatList
         horizontal={true}
-        data={data.slice(0,10)}
+        data={data}
         renderItem={({ item }) => (
           <ListAuthorsItem navigation={props.navigation} item={item} />
         )}
